74a9807446e21af38427263ba84f650a297facd8
Mikk Pristavka <mikk@fraktal.ee>
2014-11-18 19:47:52 +0200

    Initial commit of the gatherer module

---
+++ gygax/modules/gatherer.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+:mod:`gygax.modules.gatherer` --- Module to search from gatherer.wizards.com
+=================================================================
+"""
+
+import random
+
+def gatherer(bot, sender, text):
+    pass
+
+roll.command = ".gatherer"
