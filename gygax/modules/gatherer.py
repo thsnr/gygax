1aa14990c69ff1e3df81a9692cbd05630a7f8323
Mikk Pristavka <mikk@fraktal.ee>
2014-11-19 10:15:57 +0200

    Minor code style improvements

--- gygax/modules/gatherer.py
+++ gygax/modules/gatherer.py
@@ -8,23 +8,25 @@
 from urllib import request
 from bs4 import BeautifulSoup
 
+
 def gatherer(bot, sender, text):
     LIMIT = 5
     results = search(text)[0:LIMIT]
 
     if results:
-        bot.reply('First %s results:' % LIMIT)
+        bot.reply("First %s results:" % LIMIT)
         for result in results:
-            bot.reply("%s (%s)" % (result['name'], result['url']))
+            bot.reply("%s (%s)" % (result["name"], result["url"]))
     else:
-        bot.reply('No results found!')
+        bot.reply("No results found!")
 
     return
+
 
 def search(q):
     q = "".join(["+[%s]" % word for word in q.split(" ")])
 
-    base_url = 'http://gatherer.wizards.com/Pages/Search/Default.aspx'
+    base_url = "http://gatherer.wizards.com/Pages/Search/Default.aspx"
 
     try:
         req = request.Request("%s?name=%s" % (base_url, q))
@@ -36,28 +38,31 @@ def search(q):
             html = response.read()
             soup = BeautifulSoup(html)
 
-            resultsContainer = soup.find("div", attrs = {"id": 'ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_searchResultsContainer'})
+            resultsContainerId = \
+                "ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_searchResultsContainer"
+            resultsContainer = soup.find("div", attrs={"id": resultsContainerId})
+
             if resultsContainer.contents:
-                results = resultsContainer.find_all('div', attrs = { "class": 'cardInfo' })
+                results = resultsContainer.find_all("div", attrs={"class": "cardInfo"})
 
                 for result in results:
-                    name = result.find('span', {'class': 'cardTitle'}).find('a').text
-                    url = result.find('span', {'class': 'cardTitle'}).find('a')['href'].replace('../', 'http://gatherer.wizards.com/Pages/')
-                    resultsArray += [{'name': name, 'url': url}]
+                    name = result.find("span", {"class": "cardTitle"}).find("a").text
+                    url = result.find("span", {"class": "cardTitle"}).find("a")["href"]
+                    url = url.replace("../", "http://gatherer.wizards.com/Pages/")
 
+                    resultsArray += [{"name": name, "url": url}]
             return resultsArray
-        else: # request was redirected
+        else:   # request was redirected
             html = response.read()
             soup = BeautifulSoup(html)
 
             try:
                 url = response.geturl()
-                name = soup.find("div", attrs = {"class": "contentTitle"}).find("span").contents[0]
+                name = soup.find("div", attrs={"class": "contentTitle"}).find("span").contents[0]
 
-                return [{'name': name, 'url': url}]
+                return [{"name": name, "url": url}]
             except:
                 return []
-
     except:
         return []
 
